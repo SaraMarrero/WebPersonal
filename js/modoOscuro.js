0a05a1d93fa3856c5a5e7a92a1290bc28182d2b1
Sara Marrero <saramarreromiranda@gmail.com>
2023-08-07 16:05:55 +0100

    avance

--- js/modoOscuro.js
+++ js/modoOscuro.js
@@ -2,11 +2,33 @@
 const button = document.getElementById("buttonLuz");
 const body = document.getElementById("body");
 
-// Activa y desactiva el modo oscuro
-button.addEventListener('click', () => {
-    if(button.checked){
-        body.classList.add('modoOscuro');
-    } else{
-        body.classList.remove('modoOscuro');
+// Eventos
+button.addEventListener('click', modoOscuro);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modoOscuroActivo = localStorage.getItem('modoOscuro');
+    
+    if (modoOscuroActivo === 'true') {
+        activarModoOscuro();
     }
 });
+
+// Activa y desactiva el modo oscuro con el botón
+function modoOscuro() {
+    if (button.checked) {
+        body.classList.add('modoOscuro');
+        localStorage.setItem('modoOscuro', 'true');
+        button.checked = true;
+    } else {
+        body.classList.remove('modoOscuro');
+        localStorage.removeItem('modoOscuro'); 
+        button.checked = false;
+    }
+}
+
+// Activa el modo oscuro con localStorage
+function activarModoOscuro(){
+    body.classList.add('modoOscuro');
+    button.checked = false;
+}
