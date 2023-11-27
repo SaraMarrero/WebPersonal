7e1222d230e44467a7f1502c1d464887f5f94dbd
Sara Marrero <saramarreromiranda@gmail.com>
2023-11-27 18:53:36 +0000

    avance

--- js/ordenarProyectos.js
+++ js/ordenarProyectos.js
@@ -1,54 +1,48 @@
 import { arrayProyects } from "./proyectos.js";
 
 class Ordenar{
-    constructor(proyect1, proyect2, proyect3, proyect4, proyect5, proyect6){
-        this.proyect1 = proyect1;
-        this.proyect2 = proyect2;
-        this.proyect3 = proyect3;
-        this.proyect4 = proyect4;
-        this.proyect5 = proyect5;
-        this.proyect6 = proyect6;
-        this.proyects = [];
-    }
-
-    // Rellena el array con los nombres de los proyectos
-    datosArray(){
-        this.proyects = [`${this.proyect1}`, `${this.proyect2}`, `${this.proyect3}`, `${this.proyect4}`, `${this.proyect5}`, `${this.proyect6}`];
+    constructor(){
+        this.proyects = arrayProyects;
     }
 
     // Ordena los proyectos de manera ascendente
     ascendente(){
-        this.datosArray();
-        this.proyects.sort((a,b) =>  a.localeCompare(b)); 
+        this.proyects.sort((a,b) => a.nombre.localeCompare(b.nombre)); 
         return this.proyects;
     }
 
     // Ordena los proyectos de manera descendente
     descendente(){
-        this.datosArray();
-        this.proyects.sort((a,b) =>  b.localeCompare(a)); 
+        this.proyects.sort((a,b) => b.nombre.localeCompare(a.nombre)); 
         return this.proyects;
     }
 
+    limpiarHtml(article){
+        while(article.firstChild){
+            article.removeChild(article.firstChild);
+        }
+    }
+
     mostrarProyectos(){
-        this.datosArray();
+        const article = document.querySelector('.articleProyectos');
+        this.limpiarHtml(article);
 
-        this.datosArray.forEach(e => {
-            const article = document.querySelector('.articleProyectos');
-
+        this.proyects.forEach(e => {
+            console.log(e);
+        
             article.innerHTML += `
-                <div class="divProyecto1">
-                    <div class="divImgProyecto1">
-                        <img src="../img/juego1.jpg" class="imgProyecto1">
+                <div class="divProyecto${e.id}">
+                    <div class="divImgProyecto${e.id}">
+                        <img src="${e.imagen}" class="imgProyecto${e.id}">
                     </div>
 
-                    <div class="datosProyecto1">
-                        <h2 class="h2Proyecto1">Piedra papel tijera lagarto spock</h2>
-                        <p class="pProyecto1">Este proyecto va sobre el juego piedra, papel, tijera, lagarto, spock.</p>
+                    <div class="datosProyecto${e.id}">
+                        <h2 class="h2Proyecto${e.id}">${e.nombre}</h2>
+                        <p class="pProyecto${e.id}">${e.descripcion}</p>
                     
-                        <div class="enlacesProyecto1">
-                            <p><a href="https://github.com/SaraMarrero/Piedra_Papel_Tijera_Lagarto_Spock" class="aProyecto1">Código</a></p>
-                            <p><a href="https://saramarrero.github.io/Piedra_Papel_Tijera_Lagarto_Spock/" class="aProyecto1">Website</a></p>
+                        <div class="enlacesProyecto${e.id}">
+                            <p><a href="${e.codigo}" class="aProyecto${e.id}">Código</a></p>
+                            <p><a href="${e.descripcion}" class="aProyecto${e.id}">Website</a></p>
                         </div>
                     </div>
                 </div>
@@ -58,24 +52,25 @@ class Ordenar{
 }
 
 // Recoge los datos del html
-const proyect1 = document.querySelector('.h2Proyecto1');
-const proyect2 = document.querySelector('.h2Proyecto2');
-const proyect3 = document.querySelector('.h2Proyecto3');
-const proyect4 = document.querySelector('.h2Proyecto4');
-const proyect5 = document.querySelector('.h2Proyecto5');
-const proyect6 = document.querySelector('.h2Proyecto6');
-
 const select = document.querySelector('.ordenar');
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Instancia la clase
+    let option = new Ordenar();
+
+    // Muesta los proyectos
+    option.mostrarProyectos();
+})
 
 select.addEventListener('change', (e) => {
     // Instancia la clase
-    let option = new Ordenar(proyect1.textContent, proyect2.textContent, proyect3.textContent, proyect4.textContent, proyect5.textContent, proyect6.textContent);
+    let option = new Ordenar();
 
     // Si se selecciona alguna opción
     if(select.vallue !== ''){
         switch(e.target.value){
             case 'asc':
-                option.ascendente()
+                option.ascendente();
                 option.mostrarProyectos()
                 break;
             
