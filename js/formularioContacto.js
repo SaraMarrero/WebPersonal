f09003a746360c426ba2393177fa580a3734faeb
Sara Marrero <saramarreromiranda@gmail.com>
2024-01-09 20:50:13 +0000

    avance

--- js/formularioContacto.js
+++ js/formularioContacto.js
@@ -1,27 +1,11 @@
+import {nombre, email, asunto, mensaje, msgEnvio, msgError, form, pNombre, pEmail, pAsunto, pMensaje} from './variablesFormulario.js';
+
 class Validaciones {
     constructor(nombre, email, asunto, mensaje) {
         this.nombre = nombre;
         this.email = email;
         this.asunto = asunto;
         this.mensaje = mensaje;
-    }
-
-    validarDatos(input, msgError,msg) {
-        input.style.border = '1px solid red';
-
-        // modifica la clase dependiendo del mensaje
-        msgError.classList.remove('valido');
-        msgError.classList.add('error');
-
-        // agrega el mensaje al html
-        msgError.textContent = msg;
-        input.insertAdjacentElement('afterend', msgError);
-
-        // borra el aviso tras dos segundos
-        setTimeout(() => {
-            input.style.border = '2px solid #8000FF';
-            msgError.remove();
-        }, 3000);
     }
 
     // Muestra el spinner
@@ -41,10 +25,10 @@ class Contacto extends Validaciones {
     }
 
     camposCompletos(event) {
-        const regExp = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
         const table = document.querySelector('.table');
+        const regExp = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
-        if(this.nombre.value === '' && this.email.value === '' && this.asunto.value === '' && this.mensaje.value === '') {
+        if(this.nombre.value === '' && this.email.value === '' && this.asunto.value === '' && this.mensaje.value === '' || !this.email.value.match(regExp)) {
             // modifica la clase dependiendo del mensaje
             msgEnvio.classList.remove('valido');
             msgEnvio.classList.add('error');
@@ -57,52 +41,42 @@ class Contacto extends Validaciones {
                 location.href = "https://saramarrero.github.io/WebPersonal/html/contacto.html";
             }, 3000)
 
-        } else if(this.nombre.value !== '' && this.email.value !== '' && this.asunto.value !== '' && this.mensaje.value !== '') {
-            if (!this.email.value.match(regExp)) {
-                this.validarDatos(this.email, msgError,  'Formato incorrecto');
+        } else {
+            buttonEnviar.disabled = false;
+            // Muestra el spinner
+            this.showSpinner();
 
-                setTimeout(function(){
-                    location.href = "https://saramarrero.github.io/WebPersonal/html/contacto.html";
-                }, 3000)
+            // Oculta el spinner y da un aviso
+            setTimeout(() => {
+                this.hideSpinner();
 
-            } else {
-                buttonEnviar.disabled = false;
-                // Muestra el spinner
-                this.showSpinner();
+                // modifica la clase dependiendo del mensaje
+                msgEnvio.classList.remove('error');
+                msgEnvio.classList.add('valido');
 
-                // Oculta el spinner y da un aviso
-                setTimeout(() => {
-                    this.hideSpinner();
+                // agrega el mensaje al html
+                msgEnvio.textContent = 'Se envió correctamente';
+                table.insertAdjacentElement('afterend', msgEnvio);
 
-                    // modifica la clase dependiendo del mensaje
-                    msgEnvio.classList.remove('error');
-                    msgEnvio.classList.add('valido');
+                // Envía el correo electrónico
+                form.setAttribute('action', 'https://formsubmit.co/saramarreromiranda@gmail.com');
+            }, 2000);
 
-                    // agrega el mensaje al html
-                    msgEnvio.textContent = 'Se envió correctamente';
-                    table.insertAdjacentElement('afterend', msgEnvio);
-
-                    // Envía el correo electrónico
-                    form.setAttribute('action', 'https://formsubmit.co/saramarreromiranda@gmail.com');
-                }, 2000);
-
-                // Envía el formulario
-                setTimeout(() => {
-                    event.target.form.submit();
-                }, 2500);
-            }
+            // Envía el formulario
+            setTimeout(() => {
+                event.target.form.submit();
+            }, 2500);
         }
 
         // Borra el aviso y reinicia el formulario tras 4 segundos
         setTimeout(() => {
             msgEnvio.remove();
-            // form.reset();
         }, 4000);
     }
 }
 
 // Valida los campos de manera individual
-function camposVacios(input, msg){
+function camposVacios(input, msg, td){
     input.style.border = '1px solid red';
 
     // modifica la clase dependiendo del mensaje
@@ -111,10 +85,15 @@ function camposVacios(input, msg){
 
     // agrega el mensaje al html
     msgError.textContent = msg;
-    input.insertAdjacentElement('afterend', msgError);
+
+    td.appendChild(msgError);
 
     // borra el aviso tras dos segundos
-    setTimeout(() => {
+    setTimeout(function(){
+        pNombre.textContent = '';
+        pEmail.textContent = '';
+        pAsunto.textContent = '';
+        pMensaje.textContent = '';
         input.style.border = '2px solid #8000FF';
         msgError.remove();
         buttonEnviar.disabled = false;
@@ -125,37 +104,65 @@ function camposVacios(input, msg){
 document.querySelector('.loader').style.display = 'none';
 
 // Recoge los datos del html
-const nombre = document.querySelector('#nombre');
-const email = document.querySelector('#email');
-const asunto = document.querySelector('#asunto');
-const mensaje = document.querySelector('#mensaje');
-const msgEnvio = document.createElement('p');
-const msgError = document.createElement('p');
-const form = document.querySelector('.form');
-
 const buttonEnviar = document.querySelector('#enviar');
 
 // Valida el campo nombre
-nombre.addEventListener('mouseout', ()=>{
+nombre.addEventListener('keyup', ()=>{
+    
+    const tdNombre = document.querySelector('.tdNombre');
+
     if(nombre.value === ''){
-        camposVacios(nombre, 'Introduzca un nombre válido', form);
+        camposVacios(nombre, 'Introduzca un nombre válido', tdNombre);
+        pNombre.textContent = '❌'
         buttonEnviar.disabled = true;
+    }else{
+        nombre.style.border = '2px solid green';
+        pNombre.textContent = '✔️';
     }
 });
 
 // Valida el campo asunto
-asunto.addEventListener('mouseout', ()=>{
+asunto.addEventListener('keyup', ()=>{
+    
+    const tdAsunto = document.querySelector('.tdAsunto');
+
     if(asunto.value === ''){
-        camposVacios(asunto, 'Introduzca un nombre válido', form);
+        camposVacios(asunto, 'Introduzca un asunto válido', tdAsunto);
+        pAsunto.textContent = '❌'
         buttonEnviar.disabled = true;
+    } else{
+        asunto.style.border = '2px solid green';
+        pAsunto.textContent = '✔️';
     }
 });
 
 // Valida el campo mensaje
-mensaje.addEventListener('mouseout', ()=>{
+mensaje.addEventListener('keyup', ()=>{
+    
+    const tdMensaje = document.querySelector('.tdMensaje');
+
     if(mensaje.value === ''){
-        camposVacios(mensaje, 'Introduzca un nombre válido', form);
+        camposVacios(mensaje, 'Introduzca un mensaje válido', tdMensaje);
+        pMensaje.textContent = '❌'
         buttonEnviar.disabled = true;
+    }else{
+        mensaje.style.border = '2px solid green';
+        pMensaje.textContent = '✔️';
+    }
+});
+
+// Valida el campo email
+email.addEventListener('keyup', ()=>{
+    
+    const tdEmail = document.querySelector('.tdEmail');
+
+    if(email.value === ''){
+        camposVacios(email, 'Introduzca un email válido', tdEmail);
+        pEmail.textContent = '❌'
+        buttonEnviar.disabled = true;
+    }else{
+        email.style.border = '2px solid green';
+        pEmail.textContent = '✔️';
     }
 });
 
@@ -171,6 +178,10 @@ buttonEnviar.addEventListener('click', (event) => {
 
     // Reinicia el formulario 
     setTimeout(function(){
+        pNombre.textContent = '';
+        pAsunto.textContent = '';
+        pMensaje.textContent = '';
+        pEmail.textContent = '';
         form.reset();
     },3000);
 });
---
+++ js/variablesFormulario.js
@@ -0,0 +1,12 @@
+export const nombre = document.querySelector('#nombre');
+export const email = document.querySelector('#email');
+export const asunto = document.querySelector('#asunto');
+export const mensaje = document.querySelector('#mensaje');
+export const msgEnvio = document.createElement('p');
+export const msgError = document.createElement('p');
+export const form = document.querySelector('.form');
+
+export const pNombre = document.querySelector('.pNombre');
+export const pEmail = document.querySelector('.pEmail');
+export const pAsunto = document.querySelector('.pAsunto');
+export const pMensaje = document.querySelector('.pMensaje');
