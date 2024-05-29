a0b36a173a19e95008e50f7b168b6d7ebcc793ab
SaraMarrero <saramarreromiranda@gmail.com>
2024-05-29 12:19:37 +0100

    Cambio en la visualiación de los proyectos

--- js/proyectos.js
+++ js/proyectos.js
@@ -5,7 +5,8 @@ export let arrayProyects = [
         nombre: 'Piedra, papel tijera, lagarto, spock',
         descripcion: 'Este proyecto va sobre el juego piedra, papel, tijera, lagarto, spock.',
         codigo: 'https://github.com/SaraMarrero/Piedra_Papel_Tijera_Lagarto_Spock',
-        website: 'https://saramarrero.github.io/Piedra_Papel_Tijera_Lagarto_Spock/'
+        website: 'https://saramarrero.github.io/Piedra_Papel_Tijera_Lagarto_Spock/',
+        tecnologias: 'html, css, javascript, github'
     },
 
     {
@@ -14,7 +15,8 @@ export let arrayProyects = [
         nombre: 'ShareCar',
         descripcion: 'Este proyecto va sobre una web para compatir coche. En la cuál, sus principales funciones serán crear, leer, actualizar y borrar datos (CRUD).',
         codigo: 'https://github.com/SaraMarrero/ShareCar',
-        website: 'https://saramarrero.github.io/html/404.html'
+        website: 'https://saramarrero.github.io/html/404.html',
+        tecnologias: 'html, css, bootstrap, php, doxygen, github'
     },
     
     {
@@ -23,7 +25,8 @@ export let arrayProyects = [
         nombre: 'Calculadora de edad',
         descripcion: 'Este proyecto es una pequeña web donde dando una fecha de nacimiento calcula la edad exacta (años, meses y días).',
         codigo: 'https://github.com/SaraMarrero/CalculadoraEdad',
-        website: 'https://saramarrero.github.io/CalculadoraEdad/'
+        website: 'https://saramarrero.github.io/CalculadoraEdad/',
+        tecnologias: 'html, css, javascript, github'
     },
     
     {
@@ -32,7 +35,8 @@ export let arrayProyects = [
         nombre: 'IES Ana Luisa Benítez',
         descripcion: 'Rediseño de la página web del instituto IES Ana Luisa Benitez.',
         codigo: 'https://github.com/SaraMarrero/ProyectoWebBasico-IESAnaLuisaBenitez',
-        website: 'https://saramarrero.github.io/ProyectoWebBasico-IESAnaLuisaBenitez/'
+        website: 'https://saramarrero.github.io/ProyectoWebBasico-IESAnaLuisaBenitez/',
+        tecnologias: 'html, css, github'
     },
     
     {
@@ -41,7 +45,8 @@ export let arrayProyects = [
         nombre: 'Nintendo Switch',
         descripcion: 'Muestra los tipos de nintendo switch y algunos juegos que tiene o que van a salir',
         codigo: 'https://github.com/SaraMarrero/ProyectoWebBasico-NintendoSwitch',
-        website: 'https://saramarrero.github.io/ProyectoWebBasico-NintendoSwitch/'
+        website: 'https://saramarrero.github.io/ProyectoWebBasico-NintendoSwitch/',
+        tecnologias: 'html, css, javascript, github'
     },
     
     {
@@ -50,7 +55,8 @@ export let arrayProyects = [
         nombre: 'Páginas de noticias',
         descripcion: 'Este pequeño proyecto es pequeña muestra de una página de noticias. La idea del proyecto ha sido sacada de la web Frontend Mentor.',
         codigo: 'https://github.com/SaraMarrero/PaginaNoticias',
-        website: 'https://saramarrero.github.io/PaginaNoticias/'
+        website: 'https://saramarrero.github.io/PaginaNoticias/',
+        tecnologias: 'html, css, javascript, github'
     },
 
     {
@@ -59,6 +65,17 @@ export let arrayProyects = [
         nombre: 'Buscador PixabayImagenes',
         descripcion: 'Pequeño buscador de imágenes pixabay.',
         codigo: 'https://github.com/SaraMarrero/Proyecto_PixabayImagenes',
-        website: 'https://saramarrero.github.io/Proyecto_PixabayImagenes/'
+        website: 'https://saramarrero.github.io/Proyecto_PixabayImagenes/',
+        tecnologias: 'html, css, javascript, github'
+    },
+
+    {
+        id: 8,
+        imagen: '../img/gameplay.png',
+        nombre: 'Game Play',
+        descripcion: 'Web para comprar videojuegos. En el cuál, sus principales funciones serán crear, leer, actualizar y borrar datos (CRUD).',
+        codigo: 'https://github.com/SaraMarrero/Game_play',
+        website: '#',
+        tecnologias: 'html, css, javascript, github, php, tailwind, jqery, ajax, pdo, xampp'
     }
 ];
